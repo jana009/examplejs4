eeff52f0f723020e1bf70f56ac0435e1276e46d2
jana009 <61101353+jana009@users.noreply.github.com>
2020-07-29 11:54:49 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// var arrayName=["jumboo","jack","kiran"];
+// for (var name in arrayName){
+// 	if(arrayName[name].charAt(0).toUpperCase() == 'J'){
+// 		console.log(`Hello ${arrayName[name].charAt(0).toUpperCase()}${arrayName[name]}`);
+// 	}
+// 	else{
+// 	console.log('Hello '+arrayName[name]);
+//     }
+// }
+console.log('hello');
